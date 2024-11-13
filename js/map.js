f5089b2fe7df3682222b89a9e13cbe4cfd9ac5a4
mpmicheal <111928941+mpmicheal@users.noreply.github.com>
2024-11-13 02:31:37 -0600

    marker respond to clicks

--- js/map.js
+++ js/map.js
@@ -3,3 +3,15 @@ function initMap() {
     zoom: 10,
     center: { "lat": 13.93874641455393, "lng": 121.15969531596905 },
   });
+
+const marker = new google.maps.marker.AdvancedMarkerElement({
+    map,
+    position: {lat: 37.4239163, lng: -122.0947209},
+  });
+
+  marker.addListener('click', ({domEvent, latLng}) => {
+    const {target} = domEvent;
+    // Handle the click event.
+    // ...
+  });
+}
