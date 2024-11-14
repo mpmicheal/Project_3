14edaa1fb4b0e8e54c57d36b7be26f7a6e964321
mpmicheal <111928941+mpmicheal@users.noreply.github.com>
2024-11-13 18:20:20 -0600

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,17 +1,8 @@
-function initMap() {
-  var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 10,
-    center: { "lat": 41.891871832152525, "lng": -87.60507682130253 },
-  });
-
-const marker = new google.maps.marker.AdvancedMarkerElement({
-    map,
-    position: {lat: 37.4239163, lng: -122.0947209},
-  });
-
-  marker.addListener('click', ({domEvent, latLng}) => {
-    const {target} = domEvent;
-    // Handle the click event.
-    // ...
-  });
+  function initMap(){
+  var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
 }
+
+google.maps.event.addDomListener(window, 'load', initMap);
