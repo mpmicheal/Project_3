11769477894a3caee467acc7248f6f31ab7abd7e
mpmicheal <111928941+mpmicheal@users.noreply.github.com>
2024-11-14 17:40:24 -0600

    put marker on map

--- js/map.js
+++ js/map.js
@@ -3,6 +3,13 @@
       center: {lat: 40.754932, lng: -73.984016 },
       zoom: 8
     });
+
+  var marker = new google.maps.Marker({
+  position: {lat: 40.754932, lng: -73.984016},
+  map: map,
+  title: 'Your Location'
+});
+
 }
 
 google.maps.event.addDomListener(window, 'load', initMap);
