59e7073213c169d160daa6da6c31d9b5b1845860
mpmicheal <111928941+mpmicheal@users.noreply.github.com>
2024-11-14 18:13:24 -0600

    correct info window

--- js/map.js
+++ js/map.js
@@ -22,8 +22,13 @@
     "</div>";
 
   var infoWindow = new google.maps.InfoWindow({
-    content: cotentString,
+    content: contentString,
   });
+    
+  marker.addListener("click", function () {
+    infoWindow.open(map, marker);
+  });
+}
 
 }
 
